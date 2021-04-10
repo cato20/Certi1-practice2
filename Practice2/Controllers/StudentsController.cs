3b1b6d730fb60e84c4282d993526f9380d4bc369
cato20 <cae-10@hotmail.com>
2021-04-10 01:49:27 -0400

    P2-001 Implemented the endpoint of post for creating a new student.

--- Practice2/Controllers/StudentsController.cs
+++ Practice2/Controllers/StudentsController.cs
@@ -11,12 +11,24 @@ namespace Practice2.Controllers
     [Route("[controller]")]
     public class StudentsController : ControllerBase
     {
-
+        private List<Student> _studentList;
         public StudentsController()
         {
-
+            _studentList = new List<Student>();
         }
 
+        [HttpPost]
+        public Student CreateStudent([FromBody] string studentID, string studentName, string studentCarrer)
+        {
+            Student student = new Student()
+            {
+                ID = studentID,
+                Name = studentName,
+                Career = studentCarrer
+            };
+            _studentList.Add(student);
+            return student;
+        }
 
     }
 }
