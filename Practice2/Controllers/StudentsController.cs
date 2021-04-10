ab68150a2efbe0b95b3b895f507dc6852c4abef4
cato20 <cae-10@hotmail.com>
2021-04-10 01:10:45 -0400

    P2-001 Created the Student model and the StudentsController.

---
+++ Practice2/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Practice2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+
+        public StudentsController()
+        {
+
+        }
+
+
+    }
+}
---
+++ Practice2/Student.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Practice2
+{
+    public class Student
+    {
+        public String ID { get; set; }
+        public String Name { get; set; }
+        public String Career { get; set; }
+
+    }
+}
